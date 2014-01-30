eff9120302a97ad099e317b46bac17df97958125
Arturo Fernandez <arturo@bsnux.com>
2014-01-29 17:03:56 -0800

    Quick and dirty fix for getting working st sandbox app

--- moderation/forms.py
+++ moderation/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .. import get_pollers
+from social_aggregation import get_pollers
 
 
 SOURCES = [(x.name, x.title) for x in sorted(get_pollers(), key=lambda x: x.title)]
