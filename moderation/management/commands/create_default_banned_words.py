0abe6c4d4dfb8dde0a7028b164f791d44297cb56
Arturo Fernandez <arturo@bsnux.com>
2014-01-31 15:03:48 -0800

    Command changed for using new moderator classes

--- moderation/management/commands/create_default_banned_words.py
+++ moderation/management/commands/create_default_banned_words.py
@@ -1,10 +1,12 @@
 import os
 import json
 
+from fabric.colors import green
+
 from django.core.management.base import BaseCommand, CommandError
 
 from moderation.models import DefaultBannedWord
-from moderation.actions import ModerationAction
+from moderation.moderators import WordModerator
 
 
 FIXTURE_FILE = os.path.abspath(os.path.dirname(__file__)) + '/../../fixtures/moderation_banned_words.json'
@@ -14,8 +16,9 @@ class Command(BaseCommand):
     help = 'Creates default banned words'
 
     def handle(self, *args, **options):
-        actions = ModerationAction()
-        banned_words = actions.get_banned_words()
+        word_moderator = WordModerator()
+        banned_words = word_moderator.get_banned_words()
+        new_words = 0
         try:
             fixture = open(FIXTURE_FILE)
             json_data = json.load(fixture)
@@ -24,7 +27,7 @@ class Command(BaseCommand):
                 if not word in banned_words:
                     default_word = DefaultBannedWord(word=word)
                     default_word.save()
+                    new_words += 1
         except IOError:
             raise CommandError("Error opening {0} file".format(FIXTURE_FILE))
-
-        return
+        print(green("{0} new words has been added as default banned words.".format(new_words)))
