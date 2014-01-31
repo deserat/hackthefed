5b23794498da5ef342eebdbcf9f759efaee7de2b
Arturo Fernandez <arturo@bsnux.com>
2014-01-31 11:40:17 -0800

    Refactoring code creating moderators

--- moderation/actions.py
+++ moderation/actions.py
@@ -1,23 +1,60 @@
 import logging
 
-from django.db.models import signals
-
 from .models import BannedUser, BannedWord, DefaultBannedWord, FlaggedUser
 from . import get_post_model
 
 
-class ModerationActionException(Exception):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
+# Generic logger
+logger = logging.getLogger(__name__)
 
 
-class ModerationAction(object):
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
+class ModerationException(Exception):
+    '''Generic Exception for moderation actions'''
+    pass
 
+
+class UserModerator(object):
+    '''This class encapsulates actions for moderating users'''
+    def get_banned_users(self):
+        '''Returns all banned users'''
+        return BannedUser.objects.all()
+
+    def unban_user(self, poster_id, source, poster_sn=None):
+        '''Unbans given user who has been previously banned'''
+        try:
+            banned_user = BannedUser.objects.get(poster_id=poster_id, source=source,
+                                                 poster_sn=poster_sn)
+            banned_user.delete()
+        except BannedUser.DoesNotExists:
+            logger.info(u"poster_id={0}, source={1} user wasn't banned".format(poster_id, source))
+
+    def ban_user(self, poster_id, source, poster_sn=None):
+        '''Bans a given user'''
+        banned_user = BannedUser(poster_id=poster_id, source=source, poster_sn=poster_sn)
+        banned_user.save()
+
+    def flag_user(self, source, user_who, user_flagged):
+        '''Flags a user'''
+        flagged_user = FlaggedUser(poster_sn=user_flagged, source=source, who_flags=user_who)
+        flagged_user.save()
+
+    def unflag_user(self, source, user_who, user_flagged):
+        '''Unflags a user'''
+        try:
+            flagged_user = FlaggedUser.objects.get(poster_sn=user_flagged,
+                                                   source=source,
+                                                   who_flags=user_who)
+            flagged_user.delete()
+        except FlaggedUser.DoesNotExists:
+            logger.info(u"poster_id={0}, source={1} wasn't flagged".format(user_flagged, source))
+
+    def get_flagged_users(self):
+        '''Returns a list with users that has been flagged'''
+        return FlaggedUser.objects.all()
+
+
+class WordModerator(object):
+    '''This class encapsulates actions for moderating words/posts'''
     def get_banned_words(self):
         '''Return a list of all banned words. It can include
         or not default banned words. Banned words are customizable from
@@ -29,7 +66,7 @@ class ModerationAction(object):
     def set_default_banned_words(self, banned_words):
         '''Sets a list with default banned words'''
         if not isinstance(banned_words, list):
-            raise ModerationActionException('Please, use a list of banned words')
+            raise ModerationException('Please, use a list of banned words')
 
         default_banned_words = self.get_default_banned_words()
         for word in banned_words:
@@ -41,27 +78,9 @@ class ModerationAction(object):
         '''Gets a list with default banned words'''
         return set(DefaultBannedWord.objects.values_list('word', flat=True))
 
-    def get_banned_users(self):
-        '''Returns all banned users'''
-        return BannedUser.objects.all()
-
     def get_posts_with_banned_words(self):
         '''Returns a list with all posts which contains banned words'''
         pass
-
-    def unban_user(self, poster_id, source, poster_sn=None):
-        '''Unbans given user who has been previously banned'''
-        try:
-            banned_user = BannedUser.objects.get(poster_id=poster_id, source=source,
-                                                 poster_sn=poster_sn)
-            banned_user.delete()
-        except BannedUser.DoesNotExists:
-            self.logger.info(u"poster_id={0}, source={1} user wasn't banned".format(poster_id, source))
-
-    def ban_user(self, poster_id, source, poster_sn=None):
-        '''Bans a given user'''
-        banned_user = BannedUser(poster_id=poster_id, source=source, poster_sn=poster_sn)
-        banned_user.save()
 
     def passes_moderation(self, processed_data):
         '''Returns True if processed_data haven't any banned word, otherwise
@@ -88,27 +107,8 @@ class ModerationAction(object):
         banned_words = self.get_banned_words()
         return word in banned_words
 
-    def flag_user(self, source, user_who, user_flagged):
-        '''Flags a user'''
-        flagged_user = FlaggedUser(poster_sn=user_flagged, source=source, who_flags=user_who)
-        flagged_user.save()
-
-    def unflag_user(self, source, user_who, user_flagged):
-        '''Unflags a user'''
-        try:
-            flagged_user = FlaggedUser.objects.get(poster_sn=user_flagged,
-                                                   source=source,
-                                                   who_flags=user_who)
-            flagged_user.delete()
-        except FlaggedUser.DoesNotExists:
-            self.logger.info(u"poster_id={0}, source={1} wasn't flagged".format(user_flagged, source))
-
-    def get_flagged_users(self):
-        '''Returns a list with users that has been flagged'''
-        return FlaggedUser.objects.all()
-
 
 # If user is banned then it deletes his saved posts
-moderation_action = ModerationAction()
-signals.post_save.connect(moderation_action.ban_users_posts, sender=BannedUser,
-                          dispatch_uid="Remove_banned_posts")
+# moderation_action = ModerationAction()
+# signals.post_save.connect(moderation_action.ban_users_posts, sender=BannedUser,
+#                           dispatch_uid="Remove_banned_posts")
