66dcb81bdfb0a720e9a35195a4f8f0a4206bfba1
Arturo Fernandez <arturo@bsnux.com>
2014-02-05 10:40:24 +0100

    Unit tests skeleton

---
+++ moderation/tests/tests.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+
+
+class ModerationTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_approve(self):
+        pass
+
+    def test_reject(self):
+        pass
+
+    def test_flag(self):
+        pass
+
+    def test_unflag(self):
+        pass
+
+    def test_is_approved(self):
+        pass
+
+    def test_is_rejected(self):
+        pass
+
+    def test_is_pending(self):
+        pass
+
+    def test_is_flagged(self):
+        pass
