85ead5b64391a5af9f7e1c490dcd2c6cabbcc12f
Vance Dubberly <vance@themojave.com>
2014-07-03 16:32:11 -0700

    committees now import!

---
+++ import_committees.py
@@ -0,0 +1,68 @@
+import os
+import calendar
+import pymongo
+import bson
+import json
+import yaml
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = "{0}/data/congress-legislators/".format(APP_DIR)
+
+db = pymongo.MongoClient("127.0.0.1", safe=True).congress
+
+db.committee.drop()
+db.create_collection("committee")
+
+def process_committees(committees):
+
+    for c in committees:
+        exists = db.committee.find_one({"thomas_id":c["thomas_id"]})
+        if exists is not None: 
+            # if it exists it has congresses and names so we need to append
+            print "Exists"
+
+            congresses = [ "{0}".format(congress) for congress in c['congresses'] ]
+            names = [
+                { "{0}".format(key) : c['names'][key] } for key in c['names']
+            ] 
+
+            db.committee.update(
+                {"thomas_id":c["thomas_id"]},
+                {
+                    "$push": { 
+                        "congresses": { "$each": congresses } ,
+                        "names": { "$each": names } 
+                    }
+
+                }
+            )
+        else:
+            if 'congresses' in c:
+                c['congresses'] = [ "{0}".format(congress) for congress in c['congresses'] ]
+                c['names'] = [
+                    { "{0}".format(key) : c['names'][key] } for key in c['names']
+                ]    
+                if "subcommittees" in c:
+                    for sc in c["subcommittees"]:
+                        sc['congresses'] = [ "{0}".format(congress) for congress in sc['congresses'] ]
+                        sc['names'] = [
+                            { "{0}".format(key) : sc['names'][key] } for key in sc['names']
+                        ]    
+
+            else:
+                c['congresses'] = ["113"]
+                c['names'] = [{"113": c["name"]}]
+            
+            print c
+
+            db.committee.insert(c)
+
+
+with open("{0}/committees-current.yaml".format(DATA_DIR), 'r') as stream:
+    committees = yaml.load(stream) 
+    process_committees(committees)  
+
+
+with open("{0}/committees-historical.yaml".format(DATA_DIR), 'r') as stream:
+    committees = yaml.load(stream) 
+    process_committees(committees)   
