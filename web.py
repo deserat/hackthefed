20eeca3ad1aef147907b8e1b5efc21a12497b6ba
Vance Dubberly <vance@themojave.com>
2014-07-18 14:29:47 -0700

    started breaking out various objects. new templates, new functions

--- web.py
+++ web.py
@@ -14,15 +14,21 @@ app.config.update(dict(
 
 
 @app.route('/', methods=['GET'])
-def show_congressmen():
-    legislators = db.legislator.find({"sponsor_count":{"$gt":5}}) #.sort({ "sponser_count":1}).limit(10)
+def index():
+    return render_template('index.html')
+
+
+@app.route('/states', methods=['GET'])
+def states():
+
     states = db.states.find()
 
-    return render_template('index.html', legislators=legislators,states=states)
+    return render_template('states.html', states=states)
 
 
 @app.route('/committees', methods=['GET'])
 def committees():
+
     committees = db.committee.find()
 
     return render_template('committees.html', committees=committees)
@@ -38,6 +44,12 @@ def subject(subject_id):
     subject = db.subject.find_one({"_id":bson.ObjectId(subject_id)})
 
     return render_template('subject_detail.html', subject=subject)
+
+@app.route('/legislators/', methods=['GET'])
+def legislators():
+    legislators = db.legislator.find() #.sort({ "sponser_count":1}).limit(10)
+
+    return render_template('legislators.html', legislators=legislators)
 
 
 if __name__ == "__main__":
