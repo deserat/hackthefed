751b9cd8803eac0184a750cb72f4418e6a15b0ac
Vance Dubberly <vance@themojave.com>
2014-07-29 22:34:49 -0700

    playing with multiprocessing in the real file...

--- play.py
+++ play.py
@@ -4,22 +4,27 @@
 # elsewhere.
 # 
 
+# TODO: Implement multiprocessing! Would be nice to complete a job in less that 48 hours.
+
 # TODO: for legislator add subject with activity count
 # TODO: Map legislator to  congresses
 # TODO:  Map legislator to committees
 
 
-import nltk
-from nltk import bigrams
-from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
+
+
+# import nltk
+# from nltk import bigrams
+# from nltk.corpus import stopwords
+# from nltk.tokenize import RegexpTokenizer
 import operator
 import os
 import calendar
 import pymongo
 import bson
 import json
-import thread
+import multiprocessing
+
 
 
 # TODO: when backonline load by hostname or rolename
@@ -46,155 +51,168 @@ db.subject.drop()
 db.create_collection("subject")
 
 
-def get_job_dirs():
-    return []
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
 
 
-#def process_bills(dir_subset):
-# Loop over bills. Count and tally statistics on congressmen 
-# for each peace of legislation.
+def process_bills(subset):
+    """ Loop over bills. Count and tally statistics on congressmen 
+    for each peace of legislation. """
 
-for root, dirs, files in os.walk(DATA_DIR):
-    if "data.json" in files:
-        file_path = "{0}/data.json".format(root)
-        print file_path
-        bill = json.loads( open(file_path, 'r').read() )
-        
-        sponsor = bill.get('sponsor', None)
+    for subdir in subset:
+        this_dir = "{0}/{1}".format(DATA_DIR, subdir)
+        for root, dirs, files in os.walk(this_dir):
+            if "data.json" in files:
+                file_path = "{0}/data.json".format(root)
+                print file_path
+                bill = json.loads( open(file_path, 'r').read() )
+                
+                sponsor = bill.get('sponsor', None)
 
-        # Check if the bill has a sponsor if so give 'em credit
-        if sponsor and sponsor.has_key("state"):
-            db.states.update(
-                { "name" : sponsor['state'] },
-                { "$inc" : { "sponsor_count" : 1} },
-                True,
-                False
-            )
+                # Check if the bill has a sponsor if so give 'em credit
+                if sponsor and sponsor.has_key("state"):
+                    db.states.update(
+                        { "name" : sponsor['state'] },
+                        { "$inc" : { "sponsor_count" : 1} },
+                        True,
+                        False
+                    )
 
-            
-        if sponsor:
-            # some bills are sponsored by legislators some by committees
-            if sponsor.get('thomas_id', None ):
-                db.legislator.update(
-                    {"thomas_id": sponsor['thomas_id'] },
-                    {
-                        "$inc" : { 
-                            "sponsor_count" : 1,
-                        },
-                        "$push" : { "sponsored_resolutions": 
+                    
+                if sponsor:
+                    # some bills are sponsored by legislators some by committees
+                    if sponsor.get('thomas_id', None ):
+                        db.legislator.update(
+                            {"thomas_id": sponsor['thomas_id'] },
                             {
-                                "bill_id" : bill.get("bill_id", "NOID"),
-                                "title": bill.get("official_title", "TITLE") # make a fucntion that gets on of the titles
+                                "$inc" : { 
+                                    "sponsor_count" : 1,
+                                },
+                                "$push" : { "sponsored_resolutions": 
+                                    {
+                                        "bill_id" : bill.get("bill_id", "NOID"),
+                                        "title": bill.get("official_title", "TITLE") # make a fucntion that gets on of the titles
+                                    }
+                                },
                             }
-                        },
-                    }
-                )
-            else:
-                db.committee.update(
-                    {"committee_id": sponsor['committee_id'] },
-                    {
-                        "$inc" : { 
-                            "sponsor_count" : 1,
-                        },
-                        "$push" : { "sponsored_resolutions": 
+                        )
+                    else:
+                        db.committee.update(
+                            {"committee_id": sponsor['committee_id'] },
                             {
-                                "bill_id" : bill.get("bill_id", "NOID"),
-                                "title": bill.get("official_title", "TITLE") # make a fucntion that gets on of the titles
+                                "$inc" : { 
+                                    "sponsor_count" : 1,
+                                },
+                                "$push" : { "sponsored_resolutions": 
+                                    {
+                                        "bill_id" : bill.get("bill_id", "NOID"),
+                                        "title": bill.get("official_title", "TITLE") # make a fucntion that gets on of the titles
+                                    }
+                                },
                             }
-                        },
-                    }
-                )
+                        )
 
 
-        #print bill.get('subjects', None)
-        for subject in bill.get('subjects', []):
-            db.subject.update(
-                {"name": subject},
-                {
-                    "$inc": {
-                        "count": 1
-                    },
-                    "$push" : { "bills": 
+                #print bill.get('subjects', None)
+                for subject in bill.get('subjects', []):
+                    db.subject.update(
+                        {"name": subject},
                         {
-                            "bill_id" : bill.get("bill_id", "NOID"),
-                            "type" : bill.get('bill_type',None),
-                            "title": bill.get("official_title", "TITLE") # make a function that gets one of the titles
-                        }
-                    }
-                },
-                True,
-                False
-            )
-
-            if sponsor:
-                # We are interested in the subjects on which legislators are active
-                # FIXME: figure out how to do this inside the legislator document. It's damn stupid to have a seperate collection
-                print sponsor
-                try:
-                    db.legislator_subjects.update(
-                        {"thomas_id": sponsor['thomas_id'] },
-                        {
-                            "$inc" : {
-                                "{0}".format(subject) : 1
+                            "$inc": {
+                                "count": 1
+                            },
+                            "$push" : { "bills": 
+                                {
+                                    "bill_id" : bill.get("bill_id", "NOID"),
+                                    "type" : bill.get('bill_type',None),
+                                    "title": bill.get("official_title", "TITLE") # TODO:  make a function that gets one of the titles
+                                }
                             }
                         },
                         True,
                         False
                     )
-                except:
-                   print "No sponsor"
-            else:
-                print "Bill {0}".format(bill.get("official_title", "TITLE"))
+
+                    if sponsor:
+                        # We are interested in the subjects on which legislators are active
+                        # FIXME: figure out how to do this inside the legislator document. It's damn stupid to have a seperate collection
+
+                        try:
+                            db.legislator_subjects.update(
+                                {"thomas_id": sponsor['thomas_id'] },
+                                {
+                                    "$inc" : {
+                                        "{0}".format(subject) : 1
+                                    }
+                                },
+                                True,
+                                False
+                            )
+                        except:
+                           print "No sponsor"
+                    else:
+                        print "Bill {0}".format(bill.get("official_title", "TITLE"))
 
 
-            
-        #print bill.get('subjects_top_term', None)
-        if bill.get('subjects_top_term', None):
-            db.subject.update(
-                {"name": subject},
-                {
-                    "$inc": {
-                        "top_count": 1
-                    },
-                    "$push" : { "bills": 
-                    {
-                        "bill_id" : bill.get("bill_id", "NOID"),
-                        "type" : bill.get('bill_type',None),
-                        "title": bill.get("official_title", "TITLE") # make a function that gets on of the titles
-                    }
-                }
-                },
-                True,
-                False
-            )
-                
-
-        # Loop through all the cosponsors of a bill and give them each credit for the bills
-        # they cosponsor
-
-        for cosponsor in bill.get('cosponsors', ()):
-
-            db.states.update(
-                { "name" : cosponsor['state'] },
-                { "$inc" : { "cosponsor_count" : 1 } },
-                True,
-                False
-            )
-
-            db.legislator.update(
-                {"thomas_id": cosponsor['thomas_id'] },
-                {
-                    "$inc" : { "cosponsor_count" : 1},
-                    "$push" : { "cosponsored_resolutions": 
+                    
+                #print bill.get('subjects_top_term', None)
+                if bill.get('subjects_top_term', None):
+                    db.subject.update(
+                        {"name": subject},
                         {
-                            "bill_id" : bill.get("bill_id", "NOID"),
-                            "title": bill.get("official_title", "TITLE") # make a fucntion that gets on of the titles
+                            "$inc": {
+                                "top_count": 1
+                            },
+                            "$push" : { "bills": 
+                            {
+                                "bill_id" : bill.get("bill_id", "NOID"),
+                                "type" : bill.get('bill_type',None),
+                                "title": bill.get("official_title", "TITLE") # make a function that gets on of the titles
+                            }
                         }
-                    }
-                }
-            )
-            
+                        },
+                        True,
+                        False
+                    )
+                        
 
+                # Loop through all the cosponsors of a bill and give them each credit for the bills
+                # they cosponsor
+
+                for cosponsor in bill.get('cosponsors', ()):
+
+                    db.states.update(
+                        { "name" : cosponsor['state'] },
+                        { "$inc" : { "cosponsor_count" : 1 } },
+                        True,
+                        False
+                    )
+
+                    db.legislator.update(
+                        {"thomas_id": cosponsor['thomas_id'] },
+                        {
+                            "$inc" : { "cosponsor_count" : 1},
+                            "$push" : { "cosponsored_resolutions": 
+                                {
+                                    "bill_id" : bill.get("bill_id", "NOID"),
+                                    "title": bill.get("official_title", "TITLE") # make a fucntion that gets on of the titles
+                                }
+                            }
+                        }
+                    )
+                    
+if __name__ == '__main__':
+    jobs = []
+    dirs = os.walk(DATA_DIR).next()[1]
+    num = len(dirs)
+    procs = num / 4
+    for subset in list(chunks(dirs, procs)):
+        p = multiprocessing.Process(target=process_bills, args=(subset,))
+        jobs.append(p)
+        p.start()
 
 # bigram_counts = {}
 
