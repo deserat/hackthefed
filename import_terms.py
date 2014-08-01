1c01f261cd9e5c28394fa1d77d2758f70d0f9f92
Vance  Dubberly <vance@themojave.com>
2014-07-31 23:35:45 -0700

    adding sperate term import

---
+++ import_terms.py
@@ -0,0 +1,26 @@
+import os
+import pymongo
+import bson
+import json
+import csv
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = "{0}/data/congress-legislators/".format(APP_DIR)
+
+from conf.vance import DB, DB_HOST, DB_USER, DB_PASS
+
+db = pymongo.MongoClient(DB_HOST).congress
+
+db.legislator.drop()
+db.create_collection("legislator")
+
+def process_legislators(terms):
+    terms.next()
+    
+
+
+with open(DATA_DIR, 'rb') as csvfile:
+    terms = csv.reader(csvfile) 
+    terms.next()
+    for t in terms:
+        db.subject.insert( { "name" : t } )
