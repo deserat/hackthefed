df238f7e1cbe3a82969c645301447c8b340c596c
Vance Dubberly <vance.dubberly@vanced-mbp.local>
2014-12-07 15:11:39 -0800

    fixing errors from real data set

--- feed_bills_to_pandas.py
+++ feed_bills_to_pandas.py
@@ -215,7 +215,7 @@ def crawl_congress(congress):
             except Exception as e:
                     exc_type, exc_obj, exc_tb = sys.exc_info()
                     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                    print(exc_type, fname, exc_tb.tb_lineno)
+                    logger.debug(exc_type, fname, exc_tb.tb_lineno)
 
     congress_obj.legislation = pd.DataFrame(legislation)
     congress_obj.legislation.columns = [
